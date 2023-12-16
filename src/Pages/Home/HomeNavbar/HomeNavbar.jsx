64a010020e0925f047a9e61f62e4f0a2e6a5d85d
pranto <prantochandrodas@gmail.com>
2023-12-16 21:21:52 +0600

    make navbar responsive

--- src/Pages/Home/HomeNavbar/HomeNavbar.jsx
+++ src/Pages/Home/HomeNavbar/HomeNavbar.jsx
@@ -7,12 +7,18 @@ const HomeNavbar = () => {
     return (
         <div>
             <header className="bg-[#fdd670]">
-                <div className="mx-auto flex py-2 max-w-screen-xl items-center gap-8 px-2 sm:px-2 lg:px-2">
+                <div className="mx-auto flex py-2 max-w-screen-xl items-center lg:gap-8 px-2 sm:px-2 lg:px-2">
                     <div className="font-bold text-[40px]"><BsJustifyLeft /></div>
-                    <a className="block text-teal-600" href="/">
+                    <a className="lg:block hidden text-teal-600" href="/">
                         <span className="sr-only">Home</span>
                         <img src={logo} className="w-[70px]" alt="" />
                     </a>
+                    {/* large location div  */}
+                    <div className="hover:bg-[#615e58] lg:hidden block hover:bg-opacity-20 p-4 text-[#ff686e] flex justify-center items-center">
+                        <FaLocationDot />
+                        <h1 className="font-semibold text-[18px] mx-2">Dhaka</h1>
+                        <FaAngleDown />
+                    </div>
 
                     <div className="flex flex-1 items-center justify-end md:justify-between">
                         <nav aria-label="Global" className="hidden md:block">
@@ -44,12 +50,12 @@ const HomeNavbar = () => {
                         </nav>
 
                         <div className="flex items-center gap-4">
-                            {/* location div  */}
-                            <div className="hover:bg-[#615e58] hover:bg-opacity-20 p-4 text-[#ff686e] flex justify-center items-center">
-                                    <FaLocationDot />
-                                    <h1 className="font-semibold text-[18px] mx-2">Dhaka</h1>
-                                    <FaAngleDown />
-                                </div>
+                            {/* large location div  */}
+                            <div className="hover:bg-[#615e58] hidden lg:flex hover:bg-opacity-20 p-4 text-[#ff686e] flex justify-center items-center">
+                                <FaLocationDot />
+                                <h1 className="font-semibold text-[18px] mx-2">Dhaka</h1>
+                                <FaAngleDown />
+                            </div>
                             <div className="sm:flex sm:gap-4">
                                 <a
                                     className="block rounded-md bg-[#ff686e] px-5 py-2.5 text-sm font-medium text-white transition hover:bg-teal-700"
@@ -65,22 +71,6 @@ const HomeNavbar = () => {
                                     Register
                                 </a>
                             </div>
-
-                            <button
-                                className="block rounded bg-gray-100 p-2.5 text-gray-600 transition hover:text-gray-600/75 md:hidden"
-                            >
-                                <span className="sr-only">Toggle menu</span>
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    className="h-5 w-5"
-                                    fill="none"
-                                    viewBox="0 0 24 24"
-                                    stroke="currentColor"
-                                    strokeWidth="2"
-                                >
-                                    <path strokeLinecap="round" strokeLinejoin="round" d="M4 6h16M4 12h16M4 18h16" />
-                                </svg>
-                            </button>
                         </div>
                     </div>
                 </div>
